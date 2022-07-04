a78d423b2707e057e422dbe3fad43407eb300279
Michael Ekstrand <md@ekstrandom.net>
2022-07-04 10:25:49 -0600

    CLI

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,1 +1,9 @@
 //! DPCL command-line interface.
+
+use structopt::StructOpt;
+
+/// Run operations on a DPCL pipeline.
+#[derive(Debug, SturctOpt)]
+#[structopt(name="dpcl")]
+pub struct DPCLRootCommand {
+}
