feee627a3d4a87b36de6617b27eec7d53748a6bc
Michael Ekstrand <md@ekstrandom.net>
2022-07-07 14:34:30 -0600

    non-consuming task builder

--- src/pipeline/task.rs
+++ src/pipeline/task.rs
@@ -29,18 +29,46 @@ impl Task {
 
 impl TaskBuilder {
   /// Set the name for this task.
-  pub fn name<S: AsRef<str>>(mut self, name: S) -> Self {
+  pub fn name<'a, S: AsRef<str>>(&'a mut self, name: S) -> &'a mut Self {
     self.name = Some(name.as_ref().to_string());
     self
   }
 
+  /// Add a dependency for this task.
+  pub fn dependency<'a, S: AsRef<str>>(&'a mut self, dep: S) -> &'a mut Self {
+    self.dependencies.push(dep.as_ref().to_string());
+    self
+  }
+
+  /// Add an output for this task.
+  pub fn output<'a, S: AsRef<str>>(&'a mut self, dep: S) -> &'a mut Self {
+    self.outputs.push(dep.as_ref().to_string());
+    self
+  }
+
   /// Build the task.
-  pub fn build(self) -> Task {
+  pub fn build(&self) -> Task {
     Task {
-      name: self.name.expect("no name specified"),
-      command: self.command,
-      dependencies: self.dependencies,
-      outputs: self.outputs,
+      name: self.name.clone().expect("no name specified"),
+      command: self.command.clone(),
+      dependencies: self.dependencies.clone(),
+      outputs: self.outputs.clone(),
     }
   }
+}
+
+#[test]
+pub fn test_no_deps() {
+  let task = Task::builder().name("bob").build();
+  assert_eq!(task.dependencies.len(), 0);
+  assert_eq!(task.outputs.len(), 0);
+}
+
+#[test]
+pub fn test_one_dep_out() {
+  let task = Task::builder().name("bob").dependency("bob.in").output("bob.out").build();
+  assert_eq!(task.dependencies.len(), 1);
+  assert_eq!(task.dependencies[0], "bob.in");
+  assert_eq!(task.outputs.len(), 1);
+  assert_eq!(task.outputs[0], "bob.out");
 }
--- src/pipeline/test.rs
+++ src/pipeline/test.rs
@@ -1,5 +1,6 @@
 use super::*;
 
+/// Initial conditions for an empty pipeline.
 #[test]
 fn test_empty_pipeline() {
   let pipe = Pipeline::new();
@@ -9,6 +10,7 @@ fn test_empty_pipeline() {
 }
 
 
+/// Test a single task with no inputs or outputs.
 #[test]
 fn test_add_empty_task() {
   let mut pipe = Pipeline::new();
@@ -25,6 +27,28 @@ fn test_add_empty_task() {
   assert_eq!(task.name.as_str(), "bob");
   assert_eq!(task.dependencies.len(), 0);
   assert_eq!(task.outputs.len(), 0);
+
+  assert_eq!(pipe.task_dependencies("bob").len(), 0);
+  assert_eq!(pipe.task_outputs("bob").len(), 0);
+}
+
+/// Test a single task with one input and one output.
+#[test]
+fn test_add_single_inout() {
+  let mut pipe = Pipeline::new();
+
+  let task = Task::builder().name("bob").dependency("bob.in").output("bob.out").build();
+  pipe.add_task(task);
+
+  assert_eq!(pipe.task_count(), 1);
+  assert_eq!(pipe.artifact_count(), 0);
+
+  let task = pipe.get_task("bob");
+  assert!(task.is_some());
+  let task = task.expect("no task found");
+  assert_eq!(task.name.as_str(), "bob");
+  assert_eq!(task.dependencies.len(), 1);
+  assert_eq!(task.outputs.len(), 1);
 
   assert_eq!(pipe.task_dependencies("bob").len(), 0);
   assert_eq!(pipe.task_outputs("bob").len(), 0);
